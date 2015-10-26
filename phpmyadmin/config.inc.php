a9f7cc051e3bc4e5d6be78e675231a899bd9dea3
wildfireone <wildfireone@gmail.com>
2015-10-26 17:15:28 +0000

    test

--- phpmyadmin/config.inc.php
+++ phpmyadmin/config.inc.php
@@ -14,7 +14,7 @@
  * This is needed for cookie based authentication to encrypt password in
  * cookie
  */
-$cfg['blowfish_secret'] = 'ba17c1e07d65003'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+$cfg['blowfish_secret'] = ''; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
 
 /**
  * Servers configuration
