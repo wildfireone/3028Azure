cd24fa592f86bff25021367a0de6c39f395cce13
wildfireone <wildfireone@gmail.com>
2015-11-10 11:21:50 +0000

    test

--- phpmyadmin/js/error_report.js
+++ phpmyadmin/js/error_report.js
@@ -33,7 +33,7 @@ var ErrorReport = {
             //if (data.report_setting == "ask") {
               //  ErrorReport._showErrorNotification();
             //} else
-            if (data.report_setting == "always") {
+            /*if (data.report_setting == "always") {
                 report_data = ErrorReport._get_report_data(exception);
                 post_data = $.extend(report_data, {
                     send_error_report: true,
@@ -47,7 +47,7 @@ var ErrorReport = {
                         PMA_ajaxShowMessage(data.message, false);
                     }
                 });
-            }
+            }*/
         });
     },
     /**
